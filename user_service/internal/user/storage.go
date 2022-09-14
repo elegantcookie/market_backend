31179185d15ec5ae0e749f4e6944ccce13e34dbb
ecookie <gladiolud64@gmail.com>
2022-09-14 14:00:11 +0300

    method rename

--- user_service/internal/user/storage.go
+++ user_service/internal/user/storage.go
@@ -5,7 +5,7 @@ import "context"
 type Storage interface {
 	Create(ctx context.Context, user User) (string, error)
 	FindById(ctx context.Context, id string) (User, error)
-	FindByNumber(ctx context.Context, id string) (User, error)
+	FindByNumber(ctx context.Context, phoneNumber string) (User, error)
 	FindAll(ctx context.Context) ([]User, error)
 	Update(ctx context.Context, user User) error
 	Delete(ctx context.Context, id string) error
