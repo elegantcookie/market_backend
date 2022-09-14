543ec5c6a97c544710138859ccd5b1d22b704d64
ecookie <gladiolud64@gmail.com>
2022-09-14 13:55:15 +0300

    added verification code length

--- user_service/internal/user/constants.go
+++ user_service/internal/user/constants.go
@@ -7,6 +7,7 @@ import (
 const (
 	deleteRegisterNumberTime = 5 * time.Minute
 	twilioStatusApproved     = "approved"
+	verificationCodeLength   = 6
 )
 
 var verificationMessage = "Ваш код для входа в <название приложения> %s"
