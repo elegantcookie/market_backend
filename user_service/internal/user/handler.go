aa18d069764998a9c6a3845e868248f207a37bf2
ecookie <gladiolud64@gmail.com>
2022-09-14 15:51:37 +0300

    todo: handle redis errors

--- user_service/internal/user/handler.go
+++ user_service/internal/user/handler.go
@@ -14,14 +14,27 @@ type Handler struct {
 	UserService Service
 }
 
+// Endpoints paths
 const (
 	createByPhoneNumberURL = "/api/v1/users/phone"
 	sendVerificationURL    = "/api/v1/users/send_verification/:phone_number/"
+	getUsersURL            = "/api/v1/users/get/all"
+	getByIdURL             = "/api/v1/users/get/id/:id/"
+	getByPhoneNumberURL    = "/api/v1/users/get/num/:phone_number/"
+	updateByIdURL          = "/api/v1/users/upd/"
+	deleteByIdURL          = "/api/v1/users/del/id/:id/"
 )
 
+// Register adds handler functions to endpoints
 func (h *Handler) Register(router *httprouter.Router) {
 	router.HandlerFunc(http.MethodPost, createByPhoneNumberURL, apperror.Middleware(h.CreateUserByPhoneNumber))
 	router.HandlerFunc(http.MethodGet, sendVerificationURL, apperror.Middleware(h.SendVerificationCode))
+	router.HandlerFunc(http.MethodGet, getByIdURL, apperror.Middleware(h.GetUserById))
+	router.HandlerFunc(http.MethodGet, getByPhoneNumberURL, apperror.Middleware(h.GetUserByPhoneNumber))
+	router.HandlerFunc(http.MethodGet, getUsersURL, apperror.Middleware(h.GetUsers))
+	router.HandlerFunc(http.MethodPut, updateByIdURL, apperror.Middleware(h.UpdateUser))
+	router.HandlerFunc(http.MethodDelete, deleteByIdURL, apperror.Middleware(h.DeleteUser))
+
 }
 
 // CreateUserByPhoneNumber adds new user to db
@@ -68,12 +81,151 @@ func (h *Handler) SendVerificationCode(w http.ResponseWriter, r *http.Request) e
 	h.Logger.Info("SEND CODE")
 	params := r.Context().Value(httprouter.ParamsKey).(httprouter.Params)
 	phoneNumber := params.ByName("phone_number")
-	err := h.UserService.SendCode(r.Context(), phoneNumber)
+	dummyMessage, err := h.UserService.SendCode(r.Context(), phoneNumber)
 	if err != nil {
 		h.Logger.Errorf("%v", err)
 		return err
 	}
 	h.Logger.Info("OK")
 	w.WriteHeader(http.StatusOK)
+
+	// TODO delete on release
+	w.Write([]byte(dummyMessage))
+	return nil
+}
+
+// GetUserById swaggo
+// @Summary Returns user data by id
+// @Accept json
+// @Produce json
+// @Param id path string true "User ID"
+// @Tags Users
+// @Success 200
+// @Failure 400 {object} apperror.AppError
+// @Router /api/v1/users/get/id/{id} [get]
+func (h *Handler) GetUserById(w http.ResponseWriter, r *http.Request) error {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := r.Context().Value(httprouter.ParamsKey).(httprouter.Params)
+	userUUID := params.ByName("id")
+
+	user, err := h.UserService.GetById(r.Context(), userUUID)
+	if err != nil {
+		return err
+	}
+
+	userBytes, err := json.Marshal(user)
+	if err != nil {
+		return fmt.Errorf("failed to marshall user. error: %w", err)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(userBytes)
+	return nil
+}
+
+// GetUserByPhoneNumber swaggo
+// @Summary Returns user data by phone number
+// @Accept json
+// @Produce json
+// @Param phone_number path string true "Phone number"
+// @Tags Users
+// @Success 200
+// @Failure 400 {object} apperror.AppError
+// @Router /api/v1/users/get/id/{phone_number} [get]
+func (h *Handler) GetUserByPhoneNumber(w http.ResponseWriter, r *http.Request) error {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := r.Context().Value(httprouter.ParamsKey).(httprouter.Params)
+	username := params.ByName("phone_number")
+
+	user, err := h.UserService.GetByPhoneNumber(r.Context(), username)
+	if err != nil {
+		return err
+	}
+
+	userBytes, err := json.Marshal(user)
+	if err != nil {
+		return fmt.Errorf("failed to marshall user. error: %w", err)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(userBytes)
+	return nil
+}
+
+// GetUsers swaggo
+// @Summary Returns data of all users
+// @Accept json
+// @Produce json
+// @Tags Users
+// @Success 200
+// @Failure 400
+// @Router /api/v1/users/get/all [get]
+func (h *Handler) GetUsers(w http.ResponseWriter, r *http.Request) error {
+	w.Header().Set("Content-Type", "application/json")
+
+	users, err := h.UserService.GetAll(r.Context())
+	if err != nil {
+		return err
+	}
+
+	userBytes, err := json.Marshal(users)
+	if err != nil {
+		return fmt.Errorf("failed to marshall user. error: %w", err)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(userBytes)
+	return nil
+}
+
+// UpdateUser swaggo
+// @Summary Updates user data
+// @Accept json
+// @Produce json
+// @Param data body UpdateUserDTO true "update user struct"
+// @Tags Users
+// @Success 204
+// @Failure 400 {object} apperror.AppError
+// @Router /api/v1/users/upd/ [put]
+func (h *Handler) UpdateUser(w http.ResponseWriter, r *http.Request) error {
+	w.Header().Set("Content-Type", "application/json")
+
+	var dto UpdateUserDTO
+	defer r.Body.Close()
+	if err := json.NewDecoder(r.Body).Decode(&dto); err != nil {
+		return apperror.BadRequestError("invalid JSON scheme. check swagger API")
+	}
+	err := h.UserService.Update(r.Context(), dto)
+	if err != nil {
+		return err
+	}
+	w.WriteHeader(http.StatusNoContent)
+
+	return nil
+}
+
+// DeleteUser swaggo
+// @Summary Deletes user by id
+// @Accept json
+// @Produce json
+// @Param user_id path string true "User ID"
+// @Tags Users
+// @Success 204
+// @Failure 400 {object} apperror.AppError
+// @Router /api/v1/users/del/id/{id} [delete]
+func (h *Handler) DeleteUser(w http.ResponseWriter, r *http.Request) error {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := r.Context().Value(httprouter.ParamsKey).(httprouter.Params)
+	userUUID := params.ByName("id")
+
+	err := h.UserService.Delete(r.Context(), userUUID)
+	if err != nil {
+		return err
+	}
+	w.WriteHeader(http.StatusNoContent)
+
 	return nil
 }
--- user_service/internal/user/service.go
+++ user_service/internal/user/service.go
@@ -4,9 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	openapi "github.com/twilio/twilio-go/rest/api/v2010"
 	"user_service/internal/apperror"
-	"user_service/pkg/client/twilio"
 	"user_service/pkg/logging"
 )
 
@@ -28,7 +26,7 @@ func NewService(userStorage Storage, userCache Cache, logger logging.Logger) (Se
 
 type Service interface {
 	SignInByPhone(ctx context.Context, dto CreateByPhoneDTO) (string, error)
-	SendCode(ctx context.Context, phoneNumber string) error
+	SendCode(ctx context.Context, phoneNumber string) (string, error)
 	CreateByVk(ctx context.Context, dto CreateByVkDTO) (string, error)
 	GetAll(ctx context.Context) ([]User, error)
 	GetById(ctx context.Context, uuid string) (User, error)
@@ -38,6 +36,7 @@ type Service interface {
 }
 
 func (s service) SignInByPhone(ctx context.Context, dto CreateByPhoneDTO) (string, error) {
+	s.logger.Infof("dto: %+v", dto)
 	// Gets sent verification code from redis
 	val, err := s.cache.Get(ctx, dto.PhoneNumber)
 	if err != nil {
@@ -50,7 +49,7 @@ func (s service) SignInByPhone(ctx context.Context, dto CreateByPhoneDTO) (strin
 	}
 
 	// Returns id of the existing user if it is, instead of creating new one
-	if user, err := s.storage.FindByNumber(ctx, dto.PhoneNumber); err != nil {
+	if user, err := s.storage.FindByNumber(ctx, dto.PhoneNumber); err == nil {
 		return user.ID, nil
 	}
 	// Returns id of a new user if it is not
@@ -64,43 +63,61 @@ func (s service) SignInByPhone(ctx context.Context, dto CreateByPhoneDTO) (strin
 
 }
 
-func (s service) SendCode(ctx context.Context, phoneNumber string) error {
+// For dev
+func (s service) SendCode(ctx context.Context, phoneNumber string) (string, error) {
 	s.logger.Info("SEND CODE SERVICE")
-
-	// Initializes twilio client
-	client := twilio.GetClient()
-	s.logger.Info("SET UP CLIENT")
-
-	// Adds params:
-	// SetTo arg - number of user, will be sending verification code
-	// SetFrom arg - twilio number from account
-	params := &openapi.CreateMessageParams{}
-	params.SetTo(phoneNumber)
-	params.SetFrom(client.FromPhone)
 
 	code := generateVerificationCode(verificationCodeLength)
 
 	// Fills verificationMessage template with code
 	message := fmt.Sprintf(verificationMessage, code)
 
-	// Sets message text to be sent
-	params.SetBody(message)
-
-	// Creates message via twilio api
-	resp, err := client.TwilioClient.Api.CreateMessage(params)
-	if err != nil {
-		s.logger.Infof("failed to create verification: %v", err)
-		return fmt.Errorf("failed to create verification: %v", err)
-	}
-
 	// Adds to cache phoneNumber as a key and right code as a value to compare at SignInByPhone
-	err = s.cache.Set(ctx, phoneNumber, code, deleteRegisterNumberTime)
+	err := s.cache.Set(ctx, phoneNumber, code, deleteRegisterNumberTime)
 	if err != nil {
-		return err
+		return "", err
 	}
-	s.logger.Infof("Sent notification: %+v", resp)
-	return nil
+	return message, nil
 }
+
+// For prod
+//func (s service) SendCode(ctx context.Context, phoneNumber string) error {
+//	s.logger.Info("SEND CODE SERVICE")
+//
+//	// Initializes twilio client
+//	client := twilio.GetClient()
+//	s.logger.Info("SET UP CLIENT")
+//
+//	// Adds params:
+//	// SetTo arg - number of user, will be sending verification code
+//	// SetFrom arg - twilio number from account
+//	params := &openapi.CreateMessageParams{}
+//	params.SetTo(phoneNumber)
+//	params.SetFrom(client.FromPhone)
+//
+//	code := generateVerificationCode(verificationCodeLength)
+//
+//	// Fills verificationMessage template with code
+//	message := fmt.Sprintf(verificationMessage, code)
+//
+//	// Sets message text to be sent
+//	params.SetBody(message)
+//
+//	// Creates message via twilio api
+//	resp, err := client.TwilioClient.Api.CreateMessage(params)
+//	if err != nil {
+//		s.logger.Infof("failed to create verification: %v", err)
+//		return fmt.Errorf("failed to create verification: %v", err)
+//	}
+//
+//	// Adds to cache phoneNumber as a key and right code as a value to compare at SignInByPhone
+//	err = s.cache.Set(ctx, phoneNumber, code, deleteRegisterNumberTime)
+//	if err != nil {
+//		return err
+//	}
+//	s.logger.Infof("Sent notification: %+v", resp)
+//	return nil
+//}
 
 func (s service) CreateByVk(ctx context.Context, dto CreateByVkDTO) (string, error) {
 	return "", nil
@@ -124,13 +141,16 @@ func (s service) GetById(ctx context.Context, uuid string) (User, error) {
 	return user, nil
 }
 func (s service) GetByPhoneNumber(ctx context.Context, phoneNumber string) (u User, err error) {
+	s.logger.Infof("phone number: %s", phoneNumber)
 	u, err = s.storage.FindByNumber(ctx, phoneNumber)
 	if err != nil {
+		s.logger.Infof("error: %+v", err)
 		if errors.Is(err, apperror.ErrNotFound) {
-			return
+			return u, apperror.ErrNotFound
 		}
 		return u, fmt.Errorf("failed to find user by phone number: %v", err)
 	}
+	s.logger.Infof("user: %+v", u)
 	return u, nil
 }
 func (s service) Update(ctx context.Context, dto UpdateUserDTO) error {
