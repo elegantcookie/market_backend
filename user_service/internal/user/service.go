f78435a0b7123232d0decace8c97908bb11d76dc
ecookie <gladiolud64@gmail.com>
2022-09-14 13:59:02 +0300

    added CRUD + comments

--- user_service/internal/user/service.go
+++ user_service/internal/user/service.go
@@ -2,8 +2,10 @@ package user
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	openapi "github.com/twilio/twilio-go/rest/api/v2010"
+	"user_service/internal/apperror"
 	"user_service/pkg/client/twilio"
 	"user_service/pkg/logging"
 )
@@ -30,21 +32,28 @@ type Service interface {
 	CreateByVk(ctx context.Context, dto CreateByVkDTO) (string, error)
 	GetAll(ctx context.Context) ([]User, error)
 	GetById(ctx context.Context, uuid string) (User, error)
-	GetByUsername(ctx context.Context, username string) (User, error)
+	GetByPhoneNumber(ctx context.Context, phoneNumber string) (User, error)
 	Update(ctx context.Context, dto UpdateUserDTO) error
 	Delete(ctx context.Context, uuid string) error
 }
 
 func (s service) SignInByPhone(ctx context.Context, dto CreateByPhoneDTO) (string, error) {
+	// Gets sent verification code from redis
 	val, err := s.cache.Get(ctx, dto.PhoneNumber)
 	if err != nil {
 		return "", fmt.Errorf("failed to create verification check: %v", err)
 	}
+	// Compares code is correct to one, sent by user
 	if val != dto.VerificationCode {
+		// TODO remove right code from error message
 		return "", fmt.Errorf("wrong code, right one: %s", val)
 	}
 
-	// TODO check if there is an account with existing number, then just return 200
+	// Returns id of the existing user if it is, instead of creating new one
+	if user, err := s.storage.FindByNumber(ctx, dto.PhoneNumber); err != nil {
+		return user.ID, nil
+	}
+	// Returns id of a new user if it is not
 	user := NewUserByPhone(dto)
 	userID, err := s.storage.Create(ctx, user)
 	if err != nil {
@@ -58,24 +67,33 @@ func (s service) SignInByPhone(ctx context.Context, dto CreateByPhoneDTO) (strin
 func (s service) SendCode(ctx context.Context, phoneNumber string) error {
 	s.logger.Info("SEND CODE SERVICE")
 
+	// Initializes twilio client
 	client := twilio.GetClient()
 	s.logger.Info("SET UP CLIENT")
 
+	// Adds params:
+	// SetTo arg - number of user, will be sending verification code
+	// SetFrom arg - twilio number from account
 	params := &openapi.CreateMessageParams{}
 	params.SetTo(phoneNumber)
 	params.SetFrom(client.FromPhone)
 
-	code := generateVerificationCode(6)
+	code := generateVerificationCode(verificationCodeLength)
+
+	// Fills verificationMessage template with code
 	message := fmt.Sprintf(verificationMessage, code)
 
+	// Sets message text to be sent
 	params.SetBody(message)
 
+	// Creates message via twilio api
 	resp, err := client.TwilioClient.Api.CreateMessage(params)
 	if err != nil {
 		s.logger.Infof("failed to create verification: %v", err)
 		return fmt.Errorf("failed to create verification: %v", err)
 	}
 
+	// Adds to cache phoneNumber as a key and right code as a value to compare at SignInByPhone
 	err = s.cache.Set(ctx, phoneNumber, code, deleteRegisterNumberTime)
 	if err != nil {
 		return err
@@ -88,17 +106,54 @@ func (s service) CreateByVk(ctx context.Context, dto CreateByVkDTO) (string, err
 	return "", nil
 }
 func (s service) GetAll(ctx context.Context) ([]User, error) {
-	return nil, nil
+	users, err := s.storage.FindAll(ctx)
+	if err != nil {
+		return users, fmt.Errorf("failed to find users. error: %v", err)
+	}
+	return users, nil
 }
 func (s service) GetById(ctx context.Context, uuid string) (User, error) {
-	return User{}, nil
+	user, err := s.storage.FindById(ctx, uuid)
+	if err != nil {
+		// Returns 404 if an entity not found
+		if errors.Is(err, apperror.ErrNotFound) {
+			return user, err
+		}
+		return user, fmt.Errorf("failed to find user by uuid: %w", err)
+	}
+	return user, nil
 }
-func (s service) GetByUsername(ctx context.Context, username string) (User, error) {
-	return User{}, nil
+func (s service) GetByPhoneNumber(ctx context.Context, phoneNumber string) (u User, err error) {
+	u, err = s.storage.FindByNumber(ctx, phoneNumber)
+	if err != nil {
+		if errors.Is(err, apperror.ErrNotFound) {
+			return
+		}
+		return u, fmt.Errorf("failed to find user by phone number: %v", err)
+	}
+	return u, nil
 }
 func (s service) Update(ctx context.Context, dto UpdateUserDTO) error {
+	user := NewUserFromUpdateDTO(dto)
+	err := s.storage.Update(ctx, user)
+	if err != nil {
+		// Returns 404 if an entity not found
+		if errors.Is(err, apperror.ErrNotFound) {
+			return err
+		}
+		return fmt.Errorf("failed to update user. error: %w", err)
+	}
 	return nil
 }
 func (s service) Delete(ctx context.Context, uuid string) error {
-	return nil
+	err := s.storage.Delete(ctx, uuid)
+
+	if err != nil {
+		// Returns 404 if an entity not found
+		if errors.Is(err, apperror.ErrNotFound) {
+			return err
+		}
+		return fmt.Errorf("failed to delete user. error: %w", err)
+	}
+	return err
 }
