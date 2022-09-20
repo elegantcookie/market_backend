7d11367903d3eaff169a277f70a4ccf95e1c5b93
ecookie <gladiolud64@gmail.com>
2022-09-20 19:56:24 +0300

    added findByVkID method

--- user_service/internal/user/storage.go
+++ user_service/internal/user/storage.go
@@ -1,11 +1,14 @@
 package user
 
-import "context"
+import (
+	"context"
+)
 
 type Storage interface {
 	Create(ctx context.Context, user User) (string, error)
 	FindById(ctx context.Context, id string) (User, error)
 	FindByNumber(ctx context.Context, phoneNumber string) (User, error)
+	FindByVkID(ctx context.Context, vkID string) (User, error)
 	FindAll(ctx context.Context) ([]User, error)
 	Update(ctx context.Context, user User) error
 	Delete(ctx context.Context, id string) error
