93897a1dd4a0013308cba174936306081bf33f21
ecookie <gladiolud64@gmail.com>
2022-09-20 19:54:14 +0300

    added constants url and version for vk checkToken method

--- user_service/internal/user/constants.go
+++ user_service/internal/user/constants.go
@@ -8,6 +8,8 @@ const (
 	deleteRegisterNumberTime = 5 * time.Minute
 	twilioStatusApproved     = "approved"
 	verificationCodeLength   = 6
+	checkTokenURL            = "https://api.vk.com/method/secure.checkToken?token=%s&access_token=%s&v=%s"
+	checkTokenVersion        = "5.81"
 )
 
 var verificationMessage = "Ваш код для входа в <название приложения> %s"
