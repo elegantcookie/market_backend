a1ea36bd0bb1c52c987b7c6ab17f071ee9ad74b9
ecookie <gladiolud64@gmail.com>
2022-09-24 17:32:14 +0300

    now returns 401 status code if jwt is invalid

--- auth_service/internal/apperror/middleware.go
+++ auth_service/internal/apperror/middleware.go
@@ -2,22 +2,20 @@ package apperror
 
 import (
 	"errors"
-	"log"
 	"net/http"
 )
 
 type appHandler func(http.ResponseWriter, *http.Request) error
 
 func Middleware(h appHandler) http.HandlerFunc {
-	log.Println("got into middleware")
 	return func(w http.ResponseWriter, r *http.Request) {
 		var appErr *AppError
 		err := h(w, r)
 		if err != nil {
 			if errors.As(err, &appErr) {
-				if errors.Is(err, ErrNotFound) {
-					w.WriteHeader(http.StatusNotFound)
-					w.Write(ErrNotFound.Marshal())
+				if errors.Is(err, ErrInvalidTokenSignature) || errors.Is(err, ErrEmptyTokenString) {
+					w.WriteHeader(http.StatusUnauthorized)
+					w.Write(ErrInvalidTokenSignature.Marshal())
 					return
 				}
 				err := err.(*AppError)
@@ -25,7 +23,7 @@ func Middleware(h appHandler) http.HandlerFunc {
 				w.Write(err.Marshal())
 				return
 			}
-			w.WriteHeader(http.StatusTeapot)
+			w.WriteHeader(http.StatusBadRequest)
 			w.Write(systemError(err.Error()).Marshal())
 		}
 	}
