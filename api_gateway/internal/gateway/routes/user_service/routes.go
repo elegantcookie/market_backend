d3c1b447cac0b5f00e4665c32b2b5de716f28a59
ecookie <gladiolud64@gmail.com>
2022-10-05 23:14:11 +0300

    changed create by vk url

--- api_gateway/internal/gateway/routes/user_service/routes.go
+++ api_gateway/internal/gateway/routes/user_service/routes.go
@@ -2,7 +2,7 @@ package user_service
 
 const (
 	CreateByPhoneNumberURL = "/api/v1/user_service/phone"
-	CreateByVkURL          = "/api/v1/user_service/vk"
+	CreateByVkURL          = "/api/v1/user_service/vk/:vk_token/"
 	SendVerificationURL    = "/api/v1/user_service/send_verification/:phone_number/"
 	GetUsersURL            = "/api/v1/user_service/get/all"
 	GetByIdURL             = "/api/v1/user_service/get/id/:id/"
